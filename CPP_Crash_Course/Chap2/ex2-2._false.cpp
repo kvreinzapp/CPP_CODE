dbd308a0e9c9eb639a60d501425da416c38913ec
klaus <klausvonreinherz@klauss-MacBook-Air.local>
2023-03-11 22:47:20 +0800

    the excercise is kind of hard

---
+++ CPP_Crash_Course/Chap2/ex2-2._false.cpp
@@ -0,0 +1,10 @@
+// Create a struct Calculator. It should have a single constructor that takes an
+// Operation.
+#include <cstdio>
+
+struct Calculator {
+  Calculator(int condition) {}
+  int calculate(int a, int b) {}
+};
+
+int main() {}
